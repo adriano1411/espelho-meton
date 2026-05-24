bcfde8c1f31f34eb7a53741c3dcc77c00f8c2a8c
adriano141108 <adrianosud3014@gmail.com>
2026-05-24 18:58:09 -0300

    muda mensagem para fora de cobertura

--- espelho_meton.py
+++ espelho_meton.py
@@ -289,8 +289,7 @@ st.set_page_config(page_title="Busca UAPS", layout="centered")
 st.title("Espelho digital UAPS Meton de Alencar")
 
 # input antes do botão
-endereco = st.text_input("Digite o endereço:",
-                         placeholder="Ex.:rua do imperador 42")
+endereco = st.text_input("Digite o endereço:",placeholder="Ex.:rua do imperador 42")
 
 # botão do contador
 if 'contador_busca' not in st.session_state:
